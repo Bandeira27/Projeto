80ad7d5e0d27f92401f53ea7aa9c0aa45922edc1
WanderingGhosts <154605432+WanderingGhosts@users.noreply.github.com>
2024-05-20 20:01:59 -0300

    Atualizando a foto light e a animação

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     // Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("src", "./assets/Ramon-light.png");
   } else {
     // Se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/Ramon.png");
+    img.setAttribute("src", "./assets/Ramon-dark.png");
   }
 }
